3b78c7b4576f3a2bca574e75ec33211be71920bb
Alexander <AlexChapovg@gmail.com>
2025-04-10 18:46:08 +0400

    add test

--- src/categories_and_products.py
+++ src/categories_and_products.py
@@ -20,8 +20,8 @@ class Category:
     description: str  # описание
     products: list  # список товаров категории
 
-    number_of_categories = []
-    number_of_products = []
+    number_of_categories = 0
+    number_of_products = 0
 
     def __init__(self, name: str, description: str) -> None:
 
