66c432ad7c79ca12acc45b406f896f60f07ccfcb
Alexander <AlexChapovg@gmail.com>
2025-04-13 14:44:45 +0400

    isp5

--- src/categories_and_products.py
+++ src/categories_and_products.py
@@ -23,11 +23,12 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name: str, description: str, products) -> None:
-
+    def __init__(
+        self, name: str, description: str, products: list[Product]
+    ) -> None:
         self.name = name
         self.description = description
         self.products = products
 
         Category.category_count += 1
-        Category.product_count += len(products)
+        Category.product_count += sum(product.quantity for product in products)
