7842d5b69006977360d37ec174038ab43fcb6aa6
Alexander <AlexChapovg@gmail.com>
2025-04-15 08:43:48 +0400

    add test

--- src/categories_and_products.py
+++ src/categories_and_products.py
@@ -3,14 +3,42 @@ class Product:
 
     name: str  # название
     description: str  # описание
-    price: float  # цена
+    __price: float  # цена
     quantity: int  # количество в наличии
 
     def __init__(self, name: str, description: str, price: float = 0, quantity: int = 0) -> None:
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data):
+        """Принимает на вход параметры товара в словаре и возвращает созданный объект класса Product"""
+        name = product_data.get("name") or ""
+        description = product_data.get("description") or ""
+        price = product_data.get("price", 0.0)
+        quantity = product_data.get("quantity", 0)
+
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self) -> float:
+        """
+        Геттер для приватного атрибута цены.
+        """
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: float) -> None:
+        """Сеттер для приватного атрибута цены.
+        Реализует проверку:
+        в случае если цена равна или ниже нуля, выводит сообщение в консоль
+        “Цена не должна быть нулевая или отрицательная”"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            return
 
 
 class Category:
@@ -18,7 +46,7 @@ class Category:
 
     name: str  # название
     description: str  # описание
-    products: list[Product]  # список товаров категории
+    __products: list[Product]  # список товаров категории
 
     category_count = 0
     product_count = 0
@@ -26,7 +54,20 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += sum(product.quantity for product in products)
+
+    def add_product(self, product: Product) -> None:
+        """Добавляет продукт в категорию"""
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> list:
+        """Возвращает список товаров"""
+        formatted_list = []
+        for product in self.__products:
+            formatted_list.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return formatted_list
--- tests/test_categories_and_products.py
+++ tests/test_categories_and_products.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from src.categories_and_products import Category, Product
@@ -22,15 +24,29 @@ def test_init_products(products_samsung: Product) -> None:
 
 def test_set_negative_price(products_samsung: Product) -> None:
     products_samsung.price = -100.0
-    assert products_samsung.price == -100.0
+    assert products_samsung.price == 180000.0
 
 
 def test_set_zero_price(products_samsung: Product) -> None:
     products_samsung.price = 0
-    assert products_samsung.price == 0
+    assert products_samsung.price == 180000.0
 
 
 def test_category_store_products_count(products_samsung, product_iphone):
     category = Category("SmartPhones", "", [product_iphone, products_samsung])
     products_count = products_samsung.quantity + product_iphone.quantity
     assert category.product_count == products_count
+
+
+def test_new_product_creation() -> None:
+    product_data = {"name": "Test Product", "description": "Description", "price": 100.0, "quantity": 5}
+    product = Product.new_product(product_data)
+    assert product.name == "Test Product"
+    assert product.price == 100.0
+    assert product.quantity == 5
+
+
+def test_set_lower_price_without_confirm(products_samsung: Product) -> None:
+    with patch("builtins.input", return_value="n"):
+        products_samsung.price = 150000
+        assert products_samsung.price == 180000
